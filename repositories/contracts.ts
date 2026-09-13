aeb8b5a2dd74ce400cfe53459a962c3821bc6ca7
WAKASEK SARPRAS <ekonugrohodavid@gmail.com>
2026-09-13 14:55:45 +0700

    fix: resolve repository contract lint errors

--- repositories/contracts.ts
+++ repositories/contracts.ts
@@ -1,17 +1,57 @@
 import type { Repository, RepositoryId } from "./base";
 
-export interface ProfileRepository<TProfile, TCreateInput, TUpdateInput, TUserId extends RepositoryId = RepositoryId> extends Repository<TProfile, TCreateInput, TUpdateInput, TUserId> {}
+export type ProfileRepository<
+  TProfile,
+  TCreateInput,
+  TUpdateInput,
+  TUserId extends RepositoryId = RepositoryId,
+> = Repository<TProfile, TCreateInput, TUpdateInput, TUserId>;
 
-export interface ActivityRepository<TActivity, TCreateInput, TUpdateInput, TActivityId extends RepositoryId = RepositoryId> extends Repository<TActivity, TCreateInput, TUpdateInput, TActivityId> {}
+export type ActivityRepository<
+  TActivity,
+  TCreateInput,
+  TUpdateInput,
+  TActivityId extends RepositoryId = RepositoryId,
+> = Repository<TActivity, TCreateInput, TUpdateInput, TActivityId>;
 
-export interface AttemptRepository<TAttempt, TCreateInput, TUpdateInput, TAttemptId extends RepositoryId = RepositoryId> extends Repository<TAttempt, TCreateInput, TUpdateInput, TAttemptId> {}
+export type AttemptRepository<
+  TAttempt,
+  TCreateInput,
+  TUpdateInput,
+  TAttemptId extends RepositoryId = RepositoryId,
+> = Repository<TAttempt, TCreateInput, TUpdateInput, TAttemptId>;
 
-export interface CompositionRepository<TComposition, TCreateInput, TUpdateInput, TCompositionId extends RepositoryId = RepositoryId> extends Repository<TComposition, TCreateInput, TUpdateInput, TCompositionId> {}
+export type CompositionRepository<
+  TComposition,
+  TCreateInput,
+  TUpdateInput,
+  TCompositionId extends RepositoryId = RepositoryId,
+> = Repository<TComposition, TCreateInput, TUpdateInput, TCompositionId>;
 
-export interface ProgressRepository<TProgress, TCreateInput, TUpdateInput, TProgressId extends RepositoryId = RepositoryId> extends Repository<TProgress, TCreateInput, TUpdateInput, TProgressId> {}
+export type ProgressRepository<
+  TProgress,
+  TCreateInput,
+  TUpdateInput,
+  TProgressId extends RepositoryId = RepositoryId,
+> = Repository<TProgress, TCreateInput, TUpdateInput, TProgressId>;
 
-export interface MasteryRepository<TMastery, TCreateInput, TUpdateInput, TMasteryId extends RepositoryId = RepositoryId> extends Repository<TMastery, TCreateInput, TUpdateInput, TMasteryId> {}
+export type MasteryRepository<
+  TMastery,
+  TCreateInput,
+  TUpdateInput,
+  TMasteryId extends RepositoryId = RepositoryId,
+> = Repository<TMastery, TCreateInput, TUpdateInput, TMasteryId>;
 
-export interface XPRepository<TXPTransaction, TCreateInput, TUpdateInput, TXPTransactionId extends RepositoryId = RepositoryId> extends Repository<TXPTransaction, TCreateInput, TUpdateInput, TXPTransactionId> {}
+export type XPRepository<
+  TXPTransaction,
+  TCreateInput,
+  TUpdateInput,
+  TXPTransactionId extends RepositoryId = RepositoryId,
+> = Repository<TXPTransaction, TCreateInput, TUpdateInput, TXPTransactionId>;
 
-export interface AchievementRepository<TAchievement, TCreateInput, TUpdateInput, TAchievementId extends RepositoryId = RepositoryId> extends Repository<TAchievement, TCreateInput, TUpdateInput, TAchievementId> {}
+export type AchievementRepository<
+  TAchievement,
+  TCreateInput,
+  TUpdateInput,
+  TAchievementId extends RepositoryId = RepositoryId,
+> = Repository<TAchievement, TCreateInput, TUpdateInput, TAchievementId>;
