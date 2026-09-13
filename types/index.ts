1fe003f179a1bb4f13e89c518d19f3a9dc1dbcfb
WAKASEK SARPRAS <ekonugrohodavid@gmail.com>
2026-09-13 22:48:36 +0700

    feat: add core learning and music domain types

--- types/index.ts
+++ types/index.ts
@@ -15,3 +15,45 @@ export type {
   ClassStatus,
   MembershipStatus,
 } from "./class";
+
+export type {
+  Activity,
+  ActivityStatus,
+  ActivityType,
+  ContentStatus,
+  LearningContent,
+  LearningContentType,
+  PulseActivityConfiguration,
+  TempoActivityConfiguration,
+} from "./learning";
+
+export type {
+  ActivityAttempt,
+  ActivityResponse,
+  Challenge,
+  ChallengeAttempt,
+  EvaluationClassification,
+  EvaluationDimension,
+  EvaluationResult,
+  GenericActivityResponse,
+  MeterActivityResponse,
+  PulseActivityResponse,
+  RhythmActivityResponse,
+  Score,
+  TempoActivityResponse,
+  AttemptState,
+} from "./learning";
+
+export type {
+  Meter,
+  MusicComposition,
+  MusicEvent,
+  MusicEventConstraints,
+  MusicEventType,
+  MusicEventValue,
+  MusicValidationResult,
+  RhythmPattern,
+  Tempo,
+  ValidationIssue,
+  ValidationSeverity,
+} from "./music";
